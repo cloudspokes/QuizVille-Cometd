8aedae61cbcf2e68a0690ee28bac5698ac03dc51
Jeff Douglas <jeff@jeffdouglas.com>
2012-03-07 08:58:59 -0500

    changed env vars

--- config.js
+++ config.js
@@ -2,10 +2,10 @@ exports.PORT = parseInt(process.env.PORT) || 4567;
 exports.DEBUG = true; 
 
 //Salesforce  
-exports.LOGIN_SERVER = ENV['SFDC_INSTANCE_URL']; 
-exports.CLIENT_ID =   ENV['DATABASEDOTCOM_CLIENT_ID'];
-exports.CLIENT_SECRET = ENV['DATABASEDOTCOM_CLIENT_SECRET'];
+exports.LOGIN_SERVER = process.env.SFDC_INSTANCE_URL; 
+exports.CLIENT_ID = process.env.DATABASEDOTCOM_CLIENT_ID;
+exports.CLIENT_SECRET = process.env.DATABASEDOTCOM_CLIENT_SECRET;
 
-exports.USERNAME = ENV['SFDC_USERNAME'];
-exports.PASSWORD = ENV['SFDC_PASSWORD'];
+exports.USERNAME = process.env.SFDC_USERNAME;
+exports.PASSWORD = process.env.SFDC_PASSWORD;
 exports.PUSH_TOPIC = '/topic/Quiz_Answers';
